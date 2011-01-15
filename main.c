fe69a801ef25f29ce9083ba6bdbbde8f9673e618
toco <github@tobias-conradi.de>
2011-01-15 15:18:32 +0100

    "random" field but every time the same

--- main.c
+++ main.c
@@ -46,6 +46,7 @@ void makeTestData(dataStore *test)
 	}
 	
 	for(i=2;i<23;i++){
+		k=0;
 		for(j=1;j<15;j++){
 			test->hedgewood[i][j].visible=1;
 			test->hedgewood[i][j].type=(k%4)+4;
@@ -62,6 +63,55 @@ void makeTestData(dataStore *test)
 	
 }
 
+void createRandomField(dataStore *test)
+{
+	int i,j,k=0;
+	
+	/* testdata for updateGrapics */
+	for(i=0;i<2;i++){
+		for(j=0;j<16;j++){
+			test->hedgewood[i][j].visible=1;
+			test->hedgewood[i][j].type=-1;
+			test->hedgewood[i][j].aStarValue=1;
+		}
+	}
+	for(i=2;i<24;i++){
+		test->hedgewood[i][0].visible=1;
+		test->hedgewood[i][0].type=1;
+		test->hedgewood[i][0].aStarValue=-1;
+		test->hedgewood[i][15].visible=1;
+		test->hedgewood[i][15].type=2;
+		test->hedgewood[i][15].aStarValue=-1;
+	}
+	for(i=1;i<15;i++){
+		test->hedgewood[23][i].visible=1;
+		test->hedgewood[23][i].type=3;
+		test->hedgewood[23][i].aStarValue=-1;
+	}
+	srand (1);
+	for(i=2;i<23;i++){
+		for(j=1;j<15;j++){
+			k=rand()%4+4;
+			test->hedgewood[i][j].visible=1;
+			test->hedgewood[i][j].type=(k%4)+4;
+			if (k=4)
+				k=1;
+			else if(k=5)
+				k=10;
+			else if(k=6)
+				k=20;
+			else if(k=7)
+				k=30;
+			test->hedgewood[i][j].aStarValue=k;
+		}
+	}
+	test->player.p_pos.x=7;
+	test->player.p_pos.y=1;
+	test->player.p_pos.next=NULL;
+	test->player.heading=0;
+	test->player.anfang=NULL;
+	test->verticalScroll=0;
+}
 void highscoreTestdata(dataStore *store)
 {
 	char *test = "nobody";
@@ -93,7 +143,8 @@ int main(int argc, char *argv[])
 
 /* just for testing*/
  
-	makeTestData(test);
+	//makeTestData(test);
+	createRandomField(test);
 	updateGraphics(screen,test);
 	graphicLoop(screen,test);
 
