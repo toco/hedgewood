2672fa3c9de75c3b88458fc19e60dfa54faac558
toco <github@tobias-conradi.de>
2011-02-01 20:49:36 +0100

    back to macport from backup sdl_mixer still from homebrew

--- SDLincludes.h
+++ SDLincludes.h
@@ -18,9 +18,9 @@
 #define SDLINCLUDES_H
 
 #ifdef __APPLE__
-#include "/usr/local/include/SDL/SDL.h"
-#include "/usr/local/include/SDL/SDL_image.h"	/* http://www.libsdl.org/projects/SDL_image/ */
-#include "/usr/local/include/SDL/SDL_ttf.h"		/* http://www.libsdl.org/projects/SDL_ttf/ */
+#include "/opt/local/include/SDL/SDL.h"
+#include "/opt/local/include/SDL/SDL_image.h"	/* http://www.libsdl.org/projects/SDL_image/ */
+#include "/opt/local/include/SDL/SDL_ttf.h"		/* http://www.libsdl.org/projects/SDL_ttf/ */
 #include "/usr/local/include/SDL/SDL_mixer.h"
 
 #elif __WIN32__ || _MSC_VER 
